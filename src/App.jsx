55396b66e2d6254e04fd064af77e2cced914c72e
iStainCode <merinocmd@gmail.com>
2024-03-01 20:18:24 -0500

    resolver complema 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Home from "./pages/Home"
-import About from "./pages/about"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Services from "./pages/Services"
 import Contact from "./pages/Contact"
+import About from "./pages/About11"
 
 function App() {
 
